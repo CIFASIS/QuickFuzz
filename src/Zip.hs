fcdaf6068dc82e455a7710ab1f21343daa8430bd
gaa-cifasis <gg@cifasis-conicet.gov.ar>
2015-08-24 18:47:35 +0000

    Zip clean-up

--- src/Zip.hs
+++ src/Zip.hs
@@ -11,17 +11,17 @@ import Data.Binary( Binary(..), encode )
 import Codec.Archive.Zip
 
 import qualified Data.ByteString.Lazy as L
-import Data.DeriveTH
+--import Data.DeriveTH
 import Data.Word(Word8, Word16, Word32)
-import Data.Int( Int16, Int8 )
+--import Data.Int( Int16, Int8 )
 
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
 import qualified Data.Vector.Storable as VS
 
-import System.Process
-import System.Exit
-import System.Random
+--import System.Process
+--import System.Exit
+--import System.Random
 
 import Vector
 import ByteString
@@ -31,16 +31,9 @@ instance Arbitrary (V.Vector (VU.Vector Word8)) where
      l <- listOf (arbitrary :: Gen Word8)
      return $ V.replicate 32 (VU.fromList l)
 
-{-
-instance Arbitrary L.ByteString where
-   arbitrary = do 
-     l <- listOf (arbitrary :: Gen Word8)
-     return $ L.pack l
--}
-
 $(deriveArbitraryRec ''Archive)
 
 mencode :: Archive -> L.ByteString
 mencode = encode 
 
-main = quickCheckWith stdArgs { maxSuccess = 1200, maxSize = 10 } (fuzzprop "buggy_qc.zip" "/usr/bin/unzip" ["-l", "buggy_qc.zip"] mencode)
+main = quickCheckWith stdArgs { maxSuccess = 120000, maxSize = 30 } (fuzzprop "buggy_qc.zip" "/usr/bin/7z" ["l", "buggy_qc.zip"] mencode)
