d24e5137b3c4c7fa32a71129340a0c0caf5c96e5
gaa-cifasis <gg@cifasis-conicet.gov.ar>
2015-10-02 18:23:25 +0000

    moved language-javascrip to a separate repository

--- language-javascript-0.5.14.2/buildall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# First run alex on Lexer.x to generate Lexer.hs
-#./runalex.sh
-
-# do a clean build of all, including the tests
-#cabal clean && cabal configure -fbuildtests && cabal build && cabal haddock
-cabal clean && cabal configure --enable-tests && cabal build && cabal test && cabal haddock
--- packages/populate.sh
+++ packages/populate.sh
@@ -3,3 +3,4 @@ git clone https://github.com/CIFASIS/Juicy.Pixels jp
 git clone https://github.com/CIFASIS/svg-tree svg-tree
 git clone https://github.com/CIFASIS/hogg hogg
 git clone https://github.com/CIFASIS/tar tar
+git clone https://github.com/CIFASIS/language-javascript language-javascript
