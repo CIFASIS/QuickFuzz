82d07683ead3083228a05a4f262a617c6ca11246
gaa-cifasis <gg@cifasis-conicet.gov.ar>
2015-10-02 14:18:17 +0000

    removed packages

--- packages/populate.sh
+++ packages/populate.sh
@@ -1,2 +1,4 @@
 git clone https://github.com/CIFASIS/yjsvg_haskell.git yjsvg
 git clone https://github.com/CIFASIS/Juicy.Pixels jp
+git clone https://github.com/CIFASIS/svg-tree svg-tree
+git clone https://github.com/CIFASIS/hogg hogg
