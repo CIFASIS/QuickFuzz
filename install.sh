9ce33d8628a61684df075829ce93b9948a3a96b7
Martin Escarra <mescarra@gmail.com>
2015-10-06 14:47:40 -0300

    provided an installation script

---
+++ install.sh
@@ -0,0 +1,26 @@
+_PKG="yjsvg_haskell Juicy.Pixels svg-tree hogg tar language-javascript"
+_PKG_DIR="packages"
+
+# IMPORTANT ##########################
+# Add this line to your ~/.bashrc file
+export PATH=$HOME/.cabal/bin:$PATH
+######################################
+
+cabal update
+cabal install alex
+cabal install happy
+
+mkdir -p $_PKG_DIR
+cd $_PKG_DIR
+
+for i in $_PKG
+do
+    git clone https://github.com/CIFASIS/$i
+    cd $i
+    git pull
+    cabal install
+    cd ..
+done
+
+cd ..
+cabal install
