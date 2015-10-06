fce5b234abb6a0002fd569944e4afeb5ad849607
Martin Escarra <mescarra@gmail.com>
2015-10-06 15:45:50 -0300

    modified argument names and program description

--- src/Main.hs
+++ src/Main.hs
@@ -25,16 +25,16 @@ data MainArgs = MainArgs String String String String Int Int
 
 parser :: ParserSpec MainArgs
 parser = MainArgs
-    `parsedBy` reqPos          "type"        `Descr` "File Type to generate (e.g. Bmp, Ogg, Gif, ...)"
-    `andBy`    reqPos          "name"        `Descr` "Output filename"
-    `andBy`    reqPos          "command"     `Descr` "Full command line to execute"
-    `andBy`    optFlag "fuzz"  "action"      `Descr` "Action to execute (fuzz | check | gen)"
-    `andBy`    optFlag 100000000 "max-success" `Descr` "Number of attempts"
-    `andBy`    optFlag 20      "max-size"    `Descr` "Maximum size in bytes of generated values"
+    `parsedBy` reqPos            "type"        `Descr` "File Type to generate (e.g. Bmp, Ogg, Gif, ...)"
+    `andBy`    reqPos            "name"        `Descr` "Output filename"
+    `andBy`    reqPos            "command"     `Descr` "Full command line to execute"
+    `andBy`    optFlag "fuzz"    "action"      `Descr` "Action to execute (fuzz | check | gen)"
+    `andBy`    optFlag 100000000 "tries"       `Descr` "Number of attempts"
+    `andBy`    optFlag 20        "size"        `Descr` "Maximum size in bytes of generated values"
 
 cli :: IO (CmdLnInterface MainArgs)
 cli =
-    (`setAppDescr` "An experimental grammar fuzzer in Haskell using QuickCheck, Template Haskell and specific libraries from Hackage. It found interesting bugs in gdk-pixbuf, jasper and unzip.")
+    (`setAppDescr` "An experimental grammar fuzzer in Haskell using QuickCheck.")
     <$> (`setAppEpilog` "Enjoy!")
     <$> mkApp parser
 
@@ -44,14 +44,14 @@ dispatch (MainArgs t n c a succ size) = case t of
         "Gif"  -> Gif.main n c a succ size
         "Ogg"  -> Ogg.main n c a succ size
         "Zip"  -> Zip.main n c a succ size
-        "Bzip"  -> Bzip.main n c a succ size
+        "Bzip" -> Bzip.main n c a succ size
         "Tar"  -> Tar.main n c a succ size
         "Tga"  -> Tga.main n c a succ size
         "Jpeg" -> Jpeg.main n c a succ size
         "Tiff" -> Tiff.main n c a succ size
         "Xml"  -> Xml.main n c a succ size
-        "Html"  -> Html.main n c a succ size
-        "Js"  -> Js.main n c a succ size
+        "Html" -> Html.main n c a succ size
+        "Js"   -> Js.main n c a succ size
         "Png"  -> Png.main n c a succ size
         "Pnm"  -> Pnm.main n c a succ size
         "Svg"  -> Svg.main n c a succ size
