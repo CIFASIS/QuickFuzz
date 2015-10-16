3800a48a82b64c1f9d6e87eccfe3a4005ab552e7
Martin Escarra <mescarra@gmail.com>
2015-10-16 11:40:58 -0300

    Issue 2: Added a minimal installation including only JuicyPixels supported formats

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,20 @@
+{-# LANGUAGE CPP                #-}
+
 module Main where
 
-import qualified Zip
+import qualified Tiff
+import qualified Png
+import qualified Jpeg
 import qualified Bmp
 import qualified Gif
-import qualified Ogg
+
+#ifdef COMPLETE
+import qualified Zip
 import qualified Tga
+import qualified Ogg
 import qualified Tar
-import qualified Jpeg
-import qualified Tiff
 import qualified Xml
 import qualified Html
-import qualified Png
 import qualified Pnm
 import qualified Gzip
 import qualified Bzip
@@ -20,32 +24,35 @@ import qualified SimpleSvg
 import qualified Dot
 import qualified ByteString
 import qualified TTF
+#endif
 
-import System.Console.ArgParser 
+import System.Console.ArgParser
 import Args
 
 dispatch :: MainArgs -> IO ()
 dispatch args = case (findFileType args) of
 
         "Bmp"  -> Bmp.main args
-        "Dot"  -> Dot.main args
         "Gif"  -> Gif.main args
+        "Jpeg" -> Jpeg.main args
+        "Png"  -> Png.main args
+        "Tiff" -> Tiff.main args
+#ifdef COMPLETE
+        "Dot"  -> Dot.main args
         "Ogg"  -> Ogg.main args
-        "Zip"  -> Zip.main args        
+        "Zip"  -> Zip.main args
         "Bzip" -> Bzip.main args
         "Tar"  -> Tar.main args
         "Tga"  -> Tga.main args
-        "Jpeg" -> Jpeg.main args
-        "Tiff" -> Tiff.main args
         "Xml"  -> Xml.main args
         "Html" -> Html.main args
         "Js"   -> Js.main args
-        "Png"  -> Png.main args
         "Pnm"  -> Pnm.main args
         "Svg"  -> SimpleSvg.main args
         "TTF"  -> TTF.main args
-        "BS"  -> ByteString.main args
-        
+        "BS"   -> ByteString.main args
+#endif
+        _      -> print "Unsupported Type"
 
 main = do
     interface <- cli
