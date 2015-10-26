fc01bf91a9672a2fba468fe53dbcb79c6d27f83b
gustavo <g@g>
2015-10-26 17:12:31 -0300

    improved SVG support

--- src/Svg.hs
+++ src/Svg.hs
@@ -79,17 +79,60 @@ genName = listOf1 validChars :: Gen String
   where validChars = chr <$> choose (97, 122)
 
 instance Arbitrary String where
---   arbitrary = genName
-   arbitrary = oneof $ Prelude.map return ["a", "b", "c", "d", "e"]--genName
+   arbitrary = genName
+   --arbitrary = oneof $ Prelude.map return ["a", "b", "c", "d", "e"]--genName
 
 
-$(derive makeArbitrary ''Graphics.Svg.Types.Tree)
+-- $(derive makeArbitrary ''Graphics.Svg.Types.Tree)
+
+instance Arbitrary Tree where
+  arbitrary = do
+               a1 <- arbitrary
+               a2 <- arbitrary
+               a3 <- arbitrary
+               a4 <- arbitrary
+               a5 <- arbitrary
+               a6 <- arbitrary
+               a7 <- arbitrary
+               --a8 <- arbitrary
+               --a9 <- arbitrary
+               a10 <- arbitrary
+               --oneof $ Prelude.map return [PathTree a1, CircleTree a2, PolyLineTree a3, PolygonTree a4, EllipseTree a5, LineTree a6, RectangleTree a7, TextTree a8 a9, ImageTree a10] 
+               oneof $ Prelude.map return [PathTree a1, CircleTree a2, PolyLineTree a3, PolygonTree a4, EllipseTree a5, LineTree a6, RectangleTree a7,  ImageTree a10] 
+
+
 $(derive makeArbitrary ''Origin)
 
-derive makeArbitrary ''Document
+--derive makeArbitrary ''Document
+
+instance Arbitrary Document where
+   arbitrary = do
+     a1 <- arbitrary
+     a2 <- arbitrary
+     a3 <- arbitrary
+     a4 <- arbitrary
+     a5 <- arbitrary
+     a6 <- arbitrary
+     a7 <- arbitrary
+
+     return $ Document { _viewBox = a1
+                       , _width = a2
+                       , _height =  a3
+                       , _elements = a4
+                       ,_definitions = a7
+                       , _description = a5
+                       ,  _styleRules = [], _documentLocation = a6 }
+
 derive makeArbitrary ''ElementRef
 derive makeArbitrary ''Texture
-derive makeArbitrary ''DrawAttributes
+-- derive makeArbitrary ''DrawAttributes
+
+instance Arbitrary DrawAttributes where
+  arbitrary = do 
+    x1 <- arbitrary
+    x2 <- arbitrary
+    return $ mempty { _transform = x1, _strokeColor = x2 }
+
 derive makeArbitrary ''TextSpan
 derive makeArbitrary ''CssElement
 derive makeArbitrary ''Element
