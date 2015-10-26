06f3cbf6821c03ee06943ce8776f92ab8541e5fd
gustavo <g@g>
2015-10-26 17:07:12 -0300

    fixed small bug in fuzzer instalation script

--- install_opt_fuzzers.sh
+++ install_opt_fuzzers.sh
@@ -6,7 +6,7 @@ cd $_PKG_DIR
 git clone https://github.com/CIFASIS/radamsa
 cd radamsa
 git pull
-make install DESTDIR=~/.local/..
+make install DESTDIR=$HOME/.local PREFIX=""
 cd ..
 
 git clone https://github.com/CIFASIS/zzuf
