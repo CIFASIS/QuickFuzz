1c512f37cd41e42467d7141b302683e6c7b53e94
Martín <martinceresa@gmail.com>
2016-02-21 11:48:52 -0300

    poc mutt

--- src/DeriveMutation.hs
+++ src/DeriveMutation.hs
@@ -22,8 +22,25 @@ import DeriveArbitrary
 
 -- | Mutation Class
 class Arbitrary a => Mutation a where
-    mutt :: a -> a -- ^ Given a value, mutate it.
-    mut :: a
-    --  mut = unsafePerformIO $ generate $ resize 10 arbitrary
-    --  Suck it purity ?
+    mutt :: a -> Gen a -- ^ Given a value, mutate it.
+    mut :: Gen a
+    mutt = arbitrary
 
+
+howm :: Con -> (Name, Int)
+howm (NormalC n xs) = (n,length xs)
+howm (RecC n xs) = (n,length xs)
+howm (ForallC _ _ t) = howm t
+howm (InfixC _ ) = error "not yet"
+
+devMutation :: Name -> Q [Dec]
+devMutation name = do
+    def <- reify name
+    case def of -- We need constructors...
+        TyConI (DataD _ _ params constructors _) -> 
+            let fnm = mkName $ "mutt."++name in
+            return $ funD fnm $ foldl (\ p x ->
+                     let (n,cant) = howm x
+                     clouse [conP n (take cant as)] BODYQ DECQ) [] constructors
+        -- TyConI (NewtypeD _ _ params con _) -> do
+        
