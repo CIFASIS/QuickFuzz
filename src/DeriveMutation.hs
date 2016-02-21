be4c292c3c4f881962fadd4ed2a4fffc96e77a54
Martín <martinceresa@gmail.com>
2016-02-21 17:46:51 -0300

    mutation definition?

--- src/DeriveMutation.hs
+++ src/DeriveMutation.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ViewPatterns #-}
-module DeriveArbitrary where
+module DeriveMutation where
 
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
@@ -24,23 +24,47 @@ import DeriveArbitrary
 class Arbitrary a => Mutation a where
     mutt :: a -> Gen a -- ^ Given a value, mutate it.
     mut :: Gen a
-    mutt = arbitrary
+    -- mut = (arbitrary :: Gen a)
 
 
 howm :: Con -> (Name, Int)
 howm (NormalC n xs) = (n,length xs)
 howm (RecC n xs) = (n,length xs)
 howm (ForallC _ _ t) = howm t
-howm (InfixC _ ) = error "not yet"
+howm (InfixC _ _ _ ) = error "not yet"
+
+as :: [Name]
+as = map (\x -> mkName $ 'a':show x) ([1..] :: [Int])
+
+-- TODO: ViewPattern, recursive?
+-- + mutt (C a1 a2 .. an) = do 
+--          a1' <- frequency [(10,return a1), (1,arbitrary)]
+--          ...
+--          return $ C a1' a2' ...
+-- This is extremely expensive, isn't it?
+
+freqE :: Name -> ExpQ
+freqE var = appE (varE 'frequency) (listE [tupE [litE $ integerL 10, appE (varE 'return) (varE var)], tupE [litE $ integerL 1, varE 'arbitrary]])
+
+muttC :: Name -> [Name] -> ExpQ
+muttC c vars = doE $ map (\ x -> bindS (varP x) (freqE x)) vars 
+            ++ [ noBindS $ appE (varE 'return)
+                $ foldr (\x r -> appE r (varE x)) (conE c) vars]
 
 devMutation :: Name -> Q [Dec]
 devMutation name = do
     def <- reify name
     case def of -- We need constructors...
-        TyConI (DataD _ _ params constructors _) -> 
-            let fnm = mkName $ "mutt."++name in
-            return $ funD fnm $ foldl (\ p x ->
+        TyConI (DataD _ _ params constructors _) -> do
+            let fnm = mkName $ "mutt" -- ++ (showName name) 
+            f <- funD fnm $ foldl (\ p x ->
                      let (n,cant) = howm x
-                     clouse [conP n (take cant as)] BODYQ DECQ) [] constructors
+                         vars = take cant as
+                         vp = map varP vars
+                         -- vp = foldl (\vps v -> (varP v : vps, v : ves)) ([],[]) vars
+                     in
+                     (clause [conP n vp] (normalB $ muttC n vars) []) 
+                    : p) [] constructors
+            return [f]
         -- TyConI (NewtypeD _ _ params con _) -> do
         
