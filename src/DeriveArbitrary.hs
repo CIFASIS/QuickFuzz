d996e47e9af9dc053150fde1f9cae0cc2362fa94
Martin A. Ceresa <martinceresa@gmail.com>
2016-04-28 17:02:59 -0300

    ProbGen class and instance generator

--- src/DeriveArbitrary.hs
+++ src/DeriveArbitrary.hs
@@ -6,7 +6,8 @@ module DeriveArbitrary (
     module Misc,
     module ByteString,
     module Vector,
-    createIntGen
+    createIntGen,
+    devIntGen
     ) where
 
 import Megadeth.Prim
@@ -15,6 +16,10 @@ import Misc
 import ByteString
 import Vector 
 
+import ProbGen
+
+import Data.List.Split
+import Data.Maybe
 -- Gen
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
@@ -22,27 +27,33 @@ import Test.QuickCheck
 import GHC.Exts
 import GHC.Types
 -- Gen
+--
+---
 
 as :: [Name]
 as = map (\x -> mkName $ 'a':show x) ([1..] :: [Int])
 
-customFun :: Name -> [(Name,[Bool])] -> Q Dec -- Can I give the type too?
+customFun :: Name -> [(Name,[Maybe Bool])] -> Q Dec -- Can I give the type too?
 customFun fname cons = do
     let lis = mkName "xs" 
     let n = mkName "n"
+    let newl = mkName "newl"
+    let dropold = appE (appE (varE 'drop) (lift $ length cons)) (varE lis)
     let nmu = (appE (appE (varE '(-)) (varE n)) ([|1|]))
     let sizedarb = appE (appE (varE 'resize) (varE n)) $ varE 'arbitrary
     let listaFreq' = listE $ reverse $ foldl
                      (\res (c,bs) ->
                         (foldl
                             (\r b -> appE (appE (varE '(<*>)) r)
-                                (if b 
-                                     then appE (appE (varE fname) (varE lis)) nmu
-                                     else sizedarb)) (appE (varE 'pure) (conE c)) bs) : res)
+                                (case b of
+                                    Nothing -> sizedarb
+                                    Just True -> appE (appE (varE fname) (varE lis)) nmu
+                                    _ -> appE (appE (varE fname) dropold) nmu
+                                     )) (appE (varE 'pure) (conE c)) bs) : res)
                      [] cons
     let listaFreq'' = listE $ reverse $ foldl
                      (\res (c,bs) ->
-                        if and bs
+                        if (and $ map isJust bs)
                             then res else
                             (foldl (\r x -> [|$(r) <*> arbitrary |]) (appE (varE 'pure) (conE c)) bs) : res)
                      [] cons
@@ -68,29 +79,49 @@ compCust (FunD n _) =
     in
     funD nm [clause [varP s] (normalB $ appE (varE ('sized)) (appE (varE n) (varE s))) []]
 
+prefixT :: Name -> String
+prefixT n = let spt = splitOn "." $ show n
+            in concat $ init spt
+
 customG :: Name -> Q (Maybe Dec) -- Just one function
 customG name = do
     def <- reify name
+    let prefname = prefixT name
     case def of
-        TyConI (TySynD _ _ _) ->  return $ Nothing -- later
-        TyConI (DataD _ _ _ constructors _) ->
-            let fnm = mkName $ "customGen_" ++ (map (\x -> if x == '.' then '_' else
-                                                                x) $ showName name) in
-            (customFun fnm $ reverse (foldl (\p c ->  -- because foldl
-                let
-                    SimpleCon n rec vs = simpleConView n c
-                    tfs = map (\ty -> case ty of
-                                        ConT n' -> (name == n')
-                                        _ -> False) vs
-                in (n,tfs) : p) [] constructors)) >>= return . Just
+        TyConI (TySynD _ _ _) ->  return Nothing
+        TyConI (DataD _ _ params constructors _) ->
+            let fnm = mkName "prob_gen" -- "customGen_" ++ (map (\x -> if x == '.' then '_' else
+                                                                --x) $ showName name)
+                ns = map varT $ paramNames params
+                f = (customFun fnm $ reverse (foldl (\p c ->  -- because foldl
+                    let
+                        SimpleCon n rec vs = simpleConView n c
+                        tfs = map (\ty -> case ty of
+                                            ConT n' -> 
+                                                     let pren' = prefixT n'
+                                                        in
+                                                        if (n' == name) then
+                                                            Just True
+                                                        else if (prefname == pren') then
+                                                            Just False else Nothing
+                                            _ -> Nothing) vs
+                    in (n,tfs) : p) [] constructors))
+            in
+            (instanceD (cxt $ (map (appT (conT ''Arbitrary)) ns) ++ (map (appT (conT ''ProbGen)) ns))
+                                ( appT (conT ''ProbGen) (applyTo (conT name) ns))
+                                [f])
+            >>= (return . Just)
 
 createIntGen :: Name -> Q [Dec]
 createIntGen n = do
-    arb <- devArbitrary n -- n should have and arbitrary instance, and doing so we get all the dependencies as well
+    --arb <- devArbitrary n -- n should have and arbitrary instance, and doing so we get all the dependencies as well
     cstm <- customG n
     --let [FunD nm _] = cstm -- this is kinda horrible
     case cstm of
         Nothing -> (runIO $ print "Pattern not implemented") >> return []
-        Just f@(FunD nm _) -> do
-                fsized <- compCust f
-                return (arb ++ [f,fsized])
+        Just d -> return [d]
+
+isGenName = isinsName ''ProbGen
+
+devIntGen :: Name -> Q [Dec]
+devIntGen = megaderive createIntGen (const $ return False) isGenName
