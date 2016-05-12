e7ded1610558a3646e62fe9d91259343b119245f
fcostantini <fcostantini20@gmail.com>
2016-05-12 16:08:35 -0300

    more fixes on ico type (not fully working)

--- src/Ico.hs
+++ src/Ico.hs
@@ -6,7 +6,7 @@ import Images
 import Test.QuickCheck
 
 import Data.Binary( Binary(..), encode )
-import Data.Binary.Put(putWord8, putWord16le, putWord32le)
+import Data.Binary.Put
 import Codec.Picture.Bitmap
 import Codec.Picture.Types
 
@@ -31,7 +31,7 @@ data IcoEntry = IcoEntry
   { icoWidth      :: !Word8
   , icoHeight     :: !Word8
   , icoColorCount :: !Word8
-  , icoEReserved  :: !Word8
+  --, icoEReserved  :: !Word8
   , icoPlanes     :: !Word16
   , icoBitCount   :: !Word16
   , icoSize       :: !Word32
@@ -39,7 +39,7 @@ data IcoEntry = IcoEntry
   } deriving (Show, Read, Eq)
 
 --data IcoFile = IcoFile IcoHeader [IcoEntry] [([PixelRGBA8], [Pixel8])] deriving Show--(xor, and); and is 1bit (different type?)
-data IcoFile = IcoFile IcoHeader [IcoEntry] [(BmpInfoHeader, [PixelRGBA8], [Pixel8])] deriving Show
+data IcoFile = IcoFile IcoHeader [IcoEntry] [(BmpInfoHeader, Image PixelRGBA8, Image Pixel8)] deriving Show
 
 {-
 instance Arbitrary IcoFile where
@@ -74,16 +74,28 @@ instance Binary IcoEntry where
                   putWord8 $ icoWidth entry
                   putWord8 $ icoHeight entry
                   putWord8 $ icoColorCount entry
-                  putWord8 $ icoEReserved entry
+                  putWord8 $ 0--icoEReserved entry
                   putWord16le $ icoPlanes entry
                   putWord16le $ icoBitCount entry
                   putWord32le $ icoSize entry
                   putWord32le $ icoOffset entry
   get = undefined
 
-instance Binary PixelRGBA8 where
-  put (PixelRGBA8 r g b a) = put r >> put g >> put b >> put a
-  get = undefined
+--instance {-# OVERLAPPING #-} Binary [IcoEntry] where
+--  put xs = mapM_ put xs
+--  get = undefined
+
+--instance {-# OVERLAPPING #-} Binary [PixelRGBA8] where
+--  put xs = mapM_ put xs
+--  get = undefined
+
+--instance {-# OVERLAPPING #-} Binary [Pixel8] where
+--  put xs = mapM_ put xs
+--  get = undefined
+
+--instance Binary PixelRGBA8 where
+--  put (PixelRGBA8 r g b a) = put r >> put g >> put b >> put a
+--  get = undefined
 
 --empty lists?
 {-instance Binary IcoFile where
@@ -103,59 +115,43 @@ instance Binary PixelRGBA8 where
 --  get = undefined
 
 
-{-encodeBitMap :: [(BmpInfoHeader, [PixelRGBA8], [Pixel8])] -> BL.ByteString
-encodeMasks [] = BL.empty
-encodeMasks ((x,a):xs) = let ex = encodeBitmap x
-                             ea = encodeBitmap a
-                         in BL.append (BL.append ex ea) (encodeMasks xs)
-encodeIco :: IcoFile -> BL.ByteString
-encodeIco (IcoFile h es bs) = BL.append (encode h) (BL.append (encode es) (encodeMasks bs))-}
+encodeImgData :: (BmpInfoHeader, Image PixelRGBA8, Image Pixel8) -> Put
+encodeImgData (ih, xbmp, ibmp) = put ih >> bmpEncode xbmp >> bmpEncode ibmp
 
 encodeIco :: IcoFile -> BL.ByteString
-encodeIco (IcoFile h es bs) = BL.append (encode h) (BL.append (encode es) (encode bs))
+encodeIco (IcoFile h es bs) = runPut $ put h >> (mapM_ put es) >> mapM_ encodeImgData bs
 
 mencode = encodeIco
 
-{-cons4 :: V.Storable a => a -> a -> a -> a -> V.Vector a
-cons4 r g b a = V.cons r (V.cons g (V.cons b (V.singleton a)))
+andmask :: Image Pixel8
+andmask = Image 0 0 V.empty
 
-andmask :: [Pixel8]
-andmask = [1]
+whitepixel :: Image PixelRGBA8
+whitepixel = Image 1 1 (V.fromList [255, 255, 255, 255])
 
-whitepixel :: [PixelRGBA8]
-whitepixel = [PixelRGBA8 255 255 255 0]
-
-blackpixel :: [PixelRGBA8]
-blackpixel = [PixelRGBA8 0 0 0 255]
+blackpixel :: Image PixelRGBA8
+blackpixel = Image 1 1 (V.fromList [0, 0, 0, 255])
 
 filepath :: FilePath
 filepath = "/home/franco/ej.ico"
 
-ej1 :: IO ()
-ej1 = let ih        = IcoHeader {icoType = Icon, icoCount = 1}
-          eh        = [IcoEntry {icoWidth = 1, icoHeight = 1, icoColorCount = 0, icoEReserved = 0, icoPlanes  = 0, icoBitCount = 32, icoSize = 1, icoOffset = 22} ]
-          bm        = [(whitepixel, andmask)]
-          ico       = IcoFile ih eh bm
-          encoded   = mencode ico
-      in BL.writeFile filepath encoded-}
-
-andmask :: [Pixel8]
-andmask = [1]
-
-whitepixel :: [PixelRGBA8]
-whitepixel = [PixelRGBA8 255 255 255 0]
-
-blackpixel :: [PixelRGBA8]
-blackpixel = [PixelRGBA8 0 0 0 255]
-
-filepath :: FilePath
-filepath = "/home/franco/ej.ico"
+square :: Image PixelRGBA8
+square = Image 2 2 (V.fromList [0,0,255,255, 255,255,255,255, 0,255,0,255, 255,0,0,255]) --red, white, blue, gree
 
 ej1 :: IO ()
 ej1 = let ih        = IcoHeader {icoType = Icon, icoCount = 1}
-          eh        = [IcoEntry {icoWidth = 1, icoHeight = 1, icoColorCount = 0, icoEReserved = 0, icoPlanes  = 0, icoBitCount = 8, icoSize = 45, icoOffset = 22} ]
-          bh        = BmpInfoHeader {size = 40, width = 1, height = 2, planes = 1, bitPerPixel = 8, bitmapCompression = 0, byteImageSize = 5, xResolution = 0, yResolution = 0, colorCount = 0, importantColours = 0}
+          eh        = [IcoEntry {icoWidth = 1, icoHeight = 1, icoColorCount = 0, icoPlanes  = 0, icoBitCount = 32, icoSize = 44, icoOffset = 22} ]
+          bh        = BmpInfoHeader {size = 40, width = 1, height = 1, planes = 1, bitPerPixel = 32, bitmapCompression = 0, byteImageSize = 4, xResolution = 0, yResolution = 0, colorCount = 0, importantColours = 0}
           bm        = [(bh, whitepixel, andmask)]
           ico       = IcoFile ih eh bm
           encoded   = mencode ico
       in BL.writeFile filepath encoded
+
+ej2 :: IO ()
+ej2 = let ih        = IcoHeader {icoType = Icon, icoCount = 1}
+          eh        = [IcoEntry {icoWidth = 2, icoHeight = 2, icoColorCount = 0, icoPlanes  = 0, icoBitCount = 32, icoSize = 56, icoOffset = 22} ]
+          bh        = BmpInfoHeader {size = 40, width = 2, height = 2, planes = 1, bitPerPixel = 32, bitmapCompression = 0, byteImageSize = 16, xResolution = 0, yResolution = 0, colorCount = 0, importantColours = 0}
+          bm        = [(bh, square, andmask)]
+          ico       = IcoFile ih eh bm
+          encoded   = mencode ico
+      in BL.writeFile "/home/franco/sq.ico" encoded
