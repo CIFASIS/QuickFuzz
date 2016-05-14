213a1524b0a2327830255578d18f7d5f36da2331
fcostantini <fcostantini20@gmail.com>
2016-05-14 00:54:35 -0300

    ico type working, added alternative module

--- src/Ico.hs
+++ src/Ico.hs
@@ -38,7 +38,6 @@ data IcoEntry = IcoEntry
   , icoOffset     :: !Word32
   } deriving (Show, Read, Eq)
 
---data IcoFile = IcoFile IcoHeader [IcoEntry] [([PixelRGBA8], [Pixel8])] deriving Show--(xor, and); and is 1bit (different type?)
 data IcoFile = IcoFile IcoHeader [IcoEntry] [(BmpInfoHeader, Image PixelRGBA8, Image Pixel8)] deriving Show
 
 {-
@@ -81,40 +80,6 @@ instance Binary IcoEntry where
                   putWord32le $ icoOffset entry
   get = undefined
 
---instance {-# OVERLAPPING #-} Binary [IcoEntry] where
---  put xs = mapM_ put xs
---  get = undefined
-
---instance {-# OVERLAPPING #-} Binary [PixelRGBA8] where
---  put xs = mapM_ put xs
---  get = undefined
-
---instance {-# OVERLAPPING #-} Binary [Pixel8] where
---  put xs = mapM_ put xs
---  get = undefined
-
---instance Binary PixelRGBA8 where
---  put (PixelRGBA8 r g b a) = put r >> put g >> put b >> put a
---  get = undefined
-
---empty lists?
-{-instance Binary IcoFile where
-  put (IcoFile h es bs) = do
-                            put h
-                            put es
-                            put bs-}
---  put (IcoFile h es bs) = do
---                            put h
---                            put' es
---                            put'' bs
---                          where  put' []      = put (IcoEntry 0 0 0 0 0 0 0 0)
---                                 put' (e:es)  = put e >> put' es
---                                 put'' []     = put ???
---                                 put'' (b:bs) = put b >> put'' bs
-
---  get = undefined
-
-
 encodeImgData :: (BmpInfoHeader, Image PixelRGBA8, Image Pixel8) -> Put
 encodeImgData (ih, xbmp, ibmp) = put ih >> bmpEncode xbmp >> bmpEncode ibmp
 
@@ -123,8 +88,11 @@ encodeIco (IcoFile h es bs) = runPut $ put h >> (mapM_ put es) >> mapM_ encodeIm
 
 mencode = encodeIco
 
-andmask :: Image Pixel8
-andmask = Image 0 0 V.empty
+andpixel :: Image Pixel8
+andpixel = Image 1 1 (V.singleton 0)
+
+andsquare :: Image Pixel8
+andsquare = Image 2 2 (V.fromList [0, 0, 0, 0])
 
 whitepixel :: Image PixelRGBA8
 whitepixel = Image 1 1 (V.fromList [255, 255, 255, 255])
@@ -136,22 +104,22 @@ filepath :: FilePath
 filepath = "/home/franco/ej.ico"
 
 square :: Image PixelRGBA8
-square = Image 2 2 (V.fromList [0,0,255,255, 255,255,255,255, 0,255,0,255, 255,0,0,255]) --red, white, blue, gree
+square = Image 2 2 (V.fromList [0,0,255,255, 0,255,0,255, 255,0,0,255, 255,255,255,255]) --blue, green, red, white
 
 ej1 :: IO ()
 ej1 = let ih        = IcoHeader {icoType = Icon, icoCount = 1}
-          eh        = [IcoEntry {icoWidth = 1, icoHeight = 1, icoColorCount = 0, icoPlanes  = 0, icoBitCount = 32, icoSize = 44, icoOffset = 22} ]
+          eh        = [IcoEntry {icoWidth = 1, icoHeight = 1, icoColorCount = 0, icoPlanes  = 1, icoBitCount = 32, icoSize = 48, icoOffset = 22} ]
           bh        = BmpInfoHeader {size = 40, width = 1, height = 1, planes = 1, bitPerPixel = 32, bitmapCompression = 0, byteImageSize = 4, xResolution = 0, yResolution = 0, colorCount = 0, importantColours = 0}
-          bm        = [(bh, whitepixel, andmask)]
+          bm        = [(bh, blackpixel, andpixel)]
           ico       = IcoFile ih eh bm
           encoded   = mencode ico
       in BL.writeFile filepath encoded
 
 ej2 :: IO ()
 ej2 = let ih        = IcoHeader {icoType = Icon, icoCount = 1}
-          eh        = [IcoEntry {icoWidth = 2, icoHeight = 2, icoColorCount = 0, icoPlanes  = 0, icoBitCount = 32, icoSize = 56, icoOffset = 22} ]
-          bh        = BmpInfoHeader {size = 40, width = 2, height = 2, planes = 1, bitPerPixel = 32, bitmapCompression = 0, byteImageSize = 16, xResolution = 0, yResolution = 0, colorCount = 0, importantColours = 0}
-          bm        = [(bh, square, andmask)]
+          eh        = [IcoEntry {icoWidth = 2, icoHeight = 2, icoColorCount = 0, icoPlanes  = 1, icoBitCount = 32, icoSize = 64, icoOffset = 22} ]
+          bh        = BmpInfoHeader {size = 40, width = 2, height = 4, planes = 1, bitPerPixel = 32, bitmapCompression = 0, byteImageSize = 16, xResolution = 0, yResolution = 0, colorCount = 0, importantColours = 0}
+          bm        = [(bh, square, andsquare)]
           ico       = IcoFile ih eh bm
           encoded   = mencode ico
       in BL.writeFile "/home/franco/sq.ico" encoded
