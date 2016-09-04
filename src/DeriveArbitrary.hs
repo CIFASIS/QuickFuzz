7fc0e1f9c8f0a621fe54d5c32be02f863958c618
Martín <martinceresa@gmail.com>
2016-09-03 21:46:56 -0300

    instance generation Arb and Mutation

--- src/DeriveArbitrary.hs
+++ src/DeriveArbitrary.hs
@@ -2,8 +2,8 @@
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE LambdaCase #-}
 module DeriveArbitrary (
-    module Megadeth.Prim,
-    module Megadeth.DeriveArbitrary,
+--    module Megadeth.Prim,
+--    module Megadeth.DeriveArbitrary,
     module Misc,
     module ByteString,
     module Vector,
@@ -11,6 +11,8 @@ module DeriveArbitrary (
     --createIntGen,
     --instaGen,
     --devIntGen
+    devArbitrary,
+    devDeriveArbitrary 
     ) where
 
 import Megadeth.Prim
@@ -29,6 +31,126 @@ import Language.Haskell.TH.Syntax
 import Test.QuickCheck
 import GHC.Exts
 import GHC.Types
+-- Normal Arbitrary
+--
+import Control.Monad
+import Control.Arrow
+import Control.Applicative
+import Data.List
+
+-- | Build the arbitrary function with makeArbs
+chooseExpQ :: Name -> Name -> Name -> Integer -> Type -> ExpQ
+chooseExpQ g n t bf (AppT ListT ty) = [| listOf $ resize ($(varE  n) `div` 10) arbitrary |]
+chooseExpQ g n t bf ty | headOf ty /= t = [| resize (max 0 ($(varE n) - 1)) arbitrary |]
+chooseExpQ g n t bf ty =
+  case bf of
+    0  -> [| arbitrary |] 
+    1  -> [| $(varE g) $ $(varE n) - 1 |]
+    bf -> [| $(varE g) $ $(varE n) `div` bf |]
+
+
+makeArbs :: Name -> Name -> Name ->  [ConView] -> [ExpQ]
+makeArbs g n t xs = map (fmap fixAppl) [ foldl (\h ty -> uInfixE h (varE '(<*>)) (chooseExpQ g n t bf ty)) (conE name) tys' | SimpleCon name bf tys' <- xs]
+
+-- | Generic function used to create arbitrarily large tuples
+-- @
+-- do
+--  a1 <- arbitrary
+--  a2 <- arbitrary
+--  ....
+--  return $ (a1,a2,...)
+-- @
+genTupleArbs :: Int -> ExpQ
+genTupleArbs n = 
+    let ys = take n varNames
+        xs = map mkName ys
+         in
+        doE $
+             map (\x -> bindS (varP x) (varE 'arbitrary)) xs
+            ++ [ noBindS $ appE (varE 'return) (tupE (map varE xs))]
+
+
+-- | Give an arbitrary instance for its argument.
+-- It doesn't check anything, just assume that it is ok to instance
+-- its argument. And define the function arbitrary depending what type its
+-- argument references to.
+deriveArbitrary :: Name -> Q [Dec]
+deriveArbitrary t = do
+    inf <- reify t
+    runIO $ print $ "Deriving:" ++ show inf
+    case inf of
+        TyConI (DataD _ _ params constructors _) -> do
+              let ns  = map varT $ paramNames params
+                  scons = map (simpleConView t) constructors
+                  fcs = filter ((==0) . bf) scons
+                  gos g n = -- Fancy gos
+                      if length scons > 1
+                      then
+                            if length fcs == length scons
+                            then
+                                [| oneof $(listE (makeArbs g n t fcs))|]
+                            else
+                                if length fcs > 1 
+                                then
+                                    [|  if $(varE n) <= 1
+                                    then oneof $(listE (makeArbs g n t fcs))
+                                    else oneof $(listE (makeArbs g n t scons))|]
+                                else
+                                    [|  if $(varE n) <= 1
+                                    then $(head (makeArbs g n t fcs))
+                                    else oneof $(listE (makeArbs g n t scons))|]
+                      else
+                            [| $(head (makeArbs g n t scons)) |]
+              if not $ null ns then
+               [d| instance $(applyTo (tupleT (length ns)) (map (appT (conT ''Arbitrary)) ns))
+                            => Arbitrary $(applyTo (conT t) ns) where
+                              arbitrary = sized go
+                                where go n = $(gos 'go 'n) |]
+               else
+                [d| instance Arbitrary $(applyTo (conT t) ns) where
+                               arbitrary = sized go
+                                 where go n = $(gos 'go 'n)|]
+        TyConI (NewtypeD _ _ params con _) -> do 
+            let ns = map varT $ paramNames params
+                scon = simpleConView t con
+            if not $ null ns then
+               [d| instance $(applyTo (tupleT (length ns)) (map (appT (conT ''Arbitrary)) ns))
+                            => Arbitrary $(applyTo (conT t) ns) where
+                              arbitrary = sized go 
+                                where go n = $(head (makeArbs 'go 'n t [scon])) |]
+               else
+                [d| instance Arbitrary $(applyTo (conT t) ns) where
+                               arbitrary = sized go
+                                where go n = $(head (makeArbs 'go 'n t [scon])) |]
+        TyConI inp@(TySynD _ params ty) ->
+            case (getTy ty) of
+                (TupleT n) -> 
+                        let ns = map varT $ paramNames params in
+                        if not $ null ns then
+                           [d| instance $(applyTo (tupleT (length ns)) (map (appT (conT ''Arbitrary)) ns))
+                                        => Arbitrary $(applyTo (conT t) ns) where
+                                          arbitrary = $(genTupleArbs n) |]
+                        else
+                           [d| instance Arbitrary $(applyTo (conT t) ns) where
+                                          arbitrary = $(genTupleArbs n) |]
+                (ConT n) -> return [] -- This type should had been derived already,
+                                        -- It is clearly a dependency and it
+                                        -- should be put before in the topsort.
+                _ -> do
+                     runIO $ print "IGNORING"
+                     runIO $ print ty
+                     return [] 
+        d -> do
+          if (isPrim inf) then return [] else
+            (fail $ "Case not defined: " ++ show d)
+
+isArbInsName = isinsName ''Arbitrary
+
+devArbitrary :: Name -> Q [Dec]
+devArbitrary = megaderive deriveArbitrary isArbInsName 
+
+devDeriveArbitrary :: Name -> Q [Dec]
+devDeriveArbitrary = megaderive (derive makeArbitrary) isArbInsName  
 
 {-
 -- Gen
--- src/DeriveMutation.hs
+++ src/DeriveMutation.hs
@@ -1,8 +1,134 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ViewPatterns#-}
 {-# LANGUAGE FlexibleInstances,UndecidableInstances#-}
-module DeriveMutation (
-    module Megadeth.DeriveMutation
-) where
+module DeriveMutation where
 
-import Megadeth.DeriveMutation
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+
+import Test.QuickCheck
+import Control.Monad
+--import Control.Monad.Free
+--import Control.Monad.Trans.Free as FT
+import Control.Arrow
+import Control.Applicative
+import Data.List
+
+import Megadeth.Prim
+--import Mutation
+--
+-- | Mutation Class
+class  Mutation a where
+    --mutt' :: Int -> a -> Gen a
+    mutt :: a -> Gen a -- ^ Given a value, mutate it.
+    --mutt = mutt' 10
+    --mut :: Gen a
+
+instance {-#OVERLAPS#-} Mutation a => Mutation [a] where
+    mutt x = frequency $ [(20,mapM mutt x), (1, return x)]
+
+instance {-#OVERLAPS#-} Arbitrary a => Mutation a where
+    mutt a = frequency $ [ (5, return a), (1,arbitrary)]
+
+howm :: Con -> (Name, Int)
+howm (NormalC n xs) = (n,length xs)
+howm (RecC n xs) = (n,length xs)
+howm (ForallC _ _ t) = howm t
+howm (InfixC _ _ _ ) = error "not yet"
+
+as :: [Name]
+as = map (\x -> mkName $ 'a':show x) ([1..] :: [Int])
+
+-- TODO: ViewPattern, recursive?
+-- + mutt (C a1 a2 .. an) = do 
+--          a1' <- frequency [(10,return a1), (1,arbitrary), (1, mutt a1)]
+--          ...
+--          return $ C a1' a2' ...
+-- This is extremely expensive, isn't it?
+
+freqE :: Bool -> Name -> ExpQ
+freqE _ var =
+        appE
+            (varE 'frequency)
+            (listE
+                [tupE [litE $ integerL 20,
+                            (appE (varE 'mutt) (varE var))]
+                --, tupE [litE $ integerL 1, varE 'arbitrary]
+                , tupE [litE $ integerL 1, varE 'arbitrary] -- Here we could use a custom Gen
+                ])
+
+muttC :: Name -> [(Bool,Name)] -> ExpQ
+muttC c [] = 
+        appE
+            (varE 'frequency)
+            (listE
+                [
+                    tupE [litE $ integerL 20,
+                            (appE (varE 'return) (conE c)) ]
+                , tupE [litE $ integerL 1, varE 'arbitrary]
+                -- , tupE [litE $ integerL 1, varE 'mut] -- Here we could use a custom Gen
+                ])
+muttC c vars = doE $ map (\ (b,x) -> bindS (varP x) (freqE b x)) vars 
+            ++ [ noBindS $ appE (varE 'return)
+                $ foldl (\r (_,x) -> appE r (varE x)) (conE c) vars]
+
+isMutInsName = isinsName ''Mutation
+
+ifsymHeadOf :: Name -> Q Name
+ifsymHeadOf n = do
+    inf <- reify n
+    case inf of
+        TyConI (TySynD _ _ t) -> return $ headOf t
+        _ -> return n
+        
+
+devMutationRec :: Name -> Q [Dec]
+devMutationRec t = do
+    deps <- prevDev t (\_ -> return False)
+    nosym <- mapM ifsymHeadOf deps
+    let deps' = nub $ filter (not . hasArbIns) nosym  -- Get rid of all type syn ?
+    -- Just ignore typesym later... We hope that prevDev get all dependencies
+    -- all right, if not, we always have Arb => Mutation
+    --dps <- filterM isMutInsName deps' -- Arbitrary => Mutation :(
+    ds <- mapM ((flip devMutation) Nothing) deps'
+    return $ concat ds
+
+devMutation :: Name -> Maybe Name -> Q [Dec]
+devMutation name customGen = do
+    def <- reify name
+    case def of -- We need constructors...
+        TyConI (TySynD _ _ ty) -> return [] -- devMutation (headOf ty) Nothing
+        TyConI (DataD _ _ params constructors _) -> do
+            let fnm = mkName $ "mutt" -- ++ (showName name) 
+            let f = funD fnm $ foldl (\ p c ->
+                     let 
+                         SimpleCon n rec vs = simpleConView name c
+                         tfs = map (\ ty -> (countCons (== name) ty > 0)) vs
+                         vars = take (length vs) as
+                         vp = map varP vars
+                     in
+                     (clause [conP n vp] (normalB $ muttC n (zip tfs vars)) []) 
+                    : p) [] constructors
+            let ns = map varT $ paramNames params
+            if length ns > 0 then
+                case customGen of
+                    Nothing -> do
+                        dec <- instanceD (cxt $ (map (appT (conT ''Arbitrary)) ns) ++ (map (appT (conT ''Mutation)) ns))
+                                ( appT (conT ''Mutation) (applyTo (conT name) ns))
+                                [f]
+                        return [dec]
+                    Just g -> do
+                        dec <- instanceD (cxt $ (map (appT (conT ''Arbitrary)) ns) ++ (map (appT (conT ''Mutation)) ns))
+                                ( appT (conT ''Mutation) (applyTo (conT name) ns))
+                                [f]
+                        return [dec]
+            else do
+                case customGen of
+                    Nothing -> do
+                            dec <- instanceD (cxt []) [t| Mutation $(applyTo (conT name) ns) |] [f]
+                            return $ dec : []
+                    Just g -> do
+                            dec <- instanceD (cxt []) [t| Mutation $(applyTo (conT name) ns) |] [f]
+                            return $ dec : []
+        a -> return []   --return [f]
+        -- TyConI (NewtypeD _ _ params con _) -> do
