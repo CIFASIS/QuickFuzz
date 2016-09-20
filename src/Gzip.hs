ea4add32fcba02d4cff5ec0090d7feea366e030b
Lucas <lucas@lucas-desktop.(none)>
2016-09-20 11:38:25 -0300

    Necesitando ayuda

--- src/Gzip.hs
+++ src/Gzip.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TemplateHaskell, FlexibleInstances#-}
 
-module Gzip1 where
+module Gzip where
 
 --import Data.Binary( Binary(..), encode )
 
@@ -14,10 +14,10 @@ import Data.Binary
 import Data.Tuple.Select
 
 
-curry8 :: ((a,b,c,d,e,f,g,h) -> i) -> a -> b -> c -> d -> f -> g -> h -> i
+curry8 :: ((a,b,c,d,e,f,g,h) -> i) -> a -> b -> c -> d -> e -> f -> g -> h -> i
 curry8 f x1 x2 x3 x4 x5 x6 x7 x8 = f (x1,x2,x3,x4,x5,x6,x7,x8)
 
-uncurry8 :: (a -> b -> c -> d -> e -> f -> g -> h -> i) -> (a,b,c,d,e,f,g,h) -> i)
+uncurry8 :: (a -> b -> c -> d -> e -> f -> g -> h -> i) -> (a,b,c,d,e,f,g,h) -> i
 uncurry8 f p = f (sel1 p) (sel2 p) (sel3 p) (sel4 p) (sel5 p) (sel6 p) (sel7 p) (sel8 p)
  
 --type MGzipFile  = (CompressParams,L.ByteString)
@@ -25,29 +25,24 @@ type Byte = (Bool,Bool,Bool,Bool,Bool,Bool,Bool,Bool) -- Se lee de izquierda a d
 data MGzipFile = GZIP { 
 		   cm :: Byte,
 		   flg :: Byte,
-		   mtime :: (Byte,Byte,Byte,Byte)
+		   mtime :: (Byte,Byte,Byte,Byte),
 		   xfl :: Byte,
 		   crc32 :: (Byte,Byte,Byte,Byte)
-		   extras
-		   isize ::
 		 }
+		deriving(Show,Read,Eq)
 
-data ExtraFlags = XFL {
-		    xlen :: Maybe Byte
-		    crc16 :: Maybe (Byte,Byte)
-    
 instance Binary MGzipFile where
 	put gzip = do putWord8 $ 31 -- id1
 		      putWord8 $ 139
-		      putWord8 $ uncurry8 $ packWord8BE $ cm
-		      putWord8 $ uncurry8 $ packWord8BE $ flg
-		      putWord8 $ uncurry8 $ packWord8BE $ sel1 $ mtime
-		      putWord8 $ uncurry8 $ packWord8BE $ sel2 $ mtime
-		      putWord8 $ uncurry8 $ packWord8BE $ sel3 $ mtime
-		      putWord8 $ uncurry8 $ packWord8BE $ sel4 $ mtime
-		      putWord8 $ uncurry8 $ packWord8BE $ xfl
+		      putWord8 $ uncurry8 packWord8BE (cm gzip)
+		      putWord8 $ uncurry8 packWord8BE (flg gzip)
+		      putWord8 $ uncurry8 packWord8BE (sel1 $ mtime gzip)
+		      putWord8 $ uncurry8 packWord8BE (sel2 $ mtime gzip)
+		      putWord8 $ uncurry8 packWord8BE (sel3 $ mtime gzip)
+		      putWord8 $ uncurry8 packWord8BE (sel4 $ mtime gzip)
+		      putWord8 $ uncurry8 packWord8BE (xfl gzip)
 		      putWord8 $ 3 --at the moment, in unix
-		      case flg of
-			 (_,_,True,_,_,_,_,_) -> 
---mencode :: MGzipFile -> L.ByteString
---mencode (p,bs) = compressWith p bs
+	get = undefined
+
+mencode :: MGzipFile -> L.ByteString
+mencode gzip = undefined
