c86a9478cb1b63f1202241b6f16c1c3520ded88c
g <g@g>
2017-08-30 22:06:25 -0300

    enabled mutation mode in wav

--- src/Test/QuickFuzz/Gen/Media/Wav.hs
+++ src/Test/QuickFuzz/Gen/Media/Wav.hs
@@ -18,16 +18,19 @@ import Test.QuickCheck
 import Test.QuickFuzz.Derive.Arbitrary
 import Test.QuickFuzz.Derive.Show
 import Test.QuickFuzz.Derive.NFData
-
+import Test.QuickFuzz.Derive.Mutation
 import Test.QuickFuzz.Gen.FormatInfo
 import Test.QuickFuzz.Gen.Base.ByteString
 
 devArbitrary ''WaveFile
+devMutation ''WaveFile
 devNFData ''WaveFile
 
 wavInfo :: FormatInfo WaveFile NoActions
 wavInfo = def
     { encode = B.encode
+    , decode = B.decode
     , random = arbitrary
-    , ext = "wav"
+    , mutate = mutt
+    , ext = "wav" 
     }
